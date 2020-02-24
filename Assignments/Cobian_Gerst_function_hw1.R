9acb33a9e3d09fcff85e26374eb48ef451f33273
sidney gerst <you@example.com>
2020-02-23 19:52:22 -0800

    finished the assignment! Added comments and finallized the plot

--- Assignments/Cobian_Gerst_function_hw1.R
+++ Assignments/Cobian_Gerst_function_hw1.R
@@ -1,3 +1,16 @@
+
+# This function determines the amount of Rad a Bren Student has
+
+# dogs_pet coefficient is the amount of dogs obviously pet in a day default = 5
+
+# pizza coefficient is the number of whole pizzas in a day defult = 2
+
+# Stoke is the confidence gained for every completed homework assginment at Bren (confidence/hw turned in)
+
+# Hype is excitment gained for number of classes taken that quarter (excitment/class)
+
+# return rad (R)
+
 rad_brennie<- function(stoke, hype, dogs_pet = 5, pizza = 2){
   
   if(hype <= 0) return("Please be more hype")
