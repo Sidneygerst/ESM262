ec4ef31a14883697d834d17ba39f68e1d22a3f12
sidney gerst <you@example.com>
2020-02-23 19:56:41 -0800

    put the assignments into Assignment1 folder

--- Assignments/Assignment1/Cobian_Gerst_function_hw1.R
+++ Assignments/Assignment1/Cobian_Gerst_function_hw1.R
@@ -13,7 +13,7 @@
 
 rad_brennie<- function(stoke, hype, dogs_pet = 5, pizza = 2){
   
-  if(hype <= 0) return("Please be more hype")
+  if(hype <= 0) return("Please be more hype") 
   
   else if(stoke <= 0) return("Come on increase the stoke!")
 
