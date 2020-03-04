f88ceb2a2945f4629c6e03f60337634feb3c3d90
Tyler Cobian <tylercobian@169-231-155-12.wireless.ucsb.edu>
2020-03-04 11:38:10 -0800

    Other Stuff

--- Assignments/Assignment2/fishfunciton.R
+++ Assignments/Assignment2/fishfunciton.R
@@ -1,3 +1,24 @@
 #' Fish Function
 #' 
 #' 
+
+# make a data set for fish
+fish<- data.frame(
+  Species = c("Halibut", "Red Snapper", "Tuna", "Big Guy", "Little Blue"),
+  Price = c(100, 70, 90, 150, 90),
+  Santa_Barbara = c(100, 30, 50, 20, 90), 
+  Long_Beach = c(90, 10, 20, 80, 70),
+  Santa_Cruz = c(200, 80, 90, 55, 40), 
+  Portland = c(290, 50, 80, 55, 40))
+
+
+
+fish_function<- function(Location){
+  most_frequent = names(which.max(summary(fish_catch)))
+  
+    mutate_each(funs(.*Price), 3:6)
+  
+  #fish_cols = c(Santa_Barbara, Long_Beach, Santa_Cruz, Portland)
+  #revenue_location = lapply(.SD function(x) x*fish[['Price']], )
+
+} 
