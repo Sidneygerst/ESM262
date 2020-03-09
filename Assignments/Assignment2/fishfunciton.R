d4c3a7e222321fea632ce04fc3cc4007be27c037
sidney gerst <you@example.com>
2020-03-09 11:40:51 -0700

    updated plot and added parameters

--- Assignments/Assignment2/fishfunciton.R
+++ Assignments/Assignment2/fishfunciton.R
@@ -1,6 +1,10 @@
 #' Fish Function
 #' 
-#' 
+#' This function determines the fisheries revenue by location. 
+#' Make sure to have a matrix of combined fish catches per location
+#' @param fish_catch sample of total fish caught
+#' @param fish_price sample of all prices of all fish
+#' @return most_frequent_location, most_frequent_fish, revenue_location, total_revenue, plot
 
 
 ########## Set up function##############3
@@ -38,11 +42,17 @@ fish_function<- function(fish_catch, fish_price, plot = F){
   location_revenue = fish_price[,1]*fish_catch_location
   location_revenue = colSums(location_revenue)
   location_revenue_df = as.data.frame(location_revenue)
+  location_revenue_df$location = fish_locations
   
   
   if(plot){
-    fish_plot<- ggplot(location_revenue_df, aes(x = location_revenue))+
-      geom_bar()
+    fish_plot<- ggplot(location_revenue_df, aes(x = location, y = location_revenue, fill = location))+
+      geom_bar(stat = "identity")+
+      theme_bw()+
+      theme(legend.position = "none")+
+      labs(title = "Fisheries Revenue",
+           x = "Location",
+           y = "Total Revenue ($)")
   }
   else fish_plot = NULL
   
@@ -53,15 +63,8 @@ fish_function<- function(fish_catch, fish_price, plot = F){
               total_revenue = total_revenue,
               plot = fish_plot))
   
-  
-
-  
   }
   
-  
-  
-  
-fish_function(fish_catch, fish_price, plot = T)
 
 
 
