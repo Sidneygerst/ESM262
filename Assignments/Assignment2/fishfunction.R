9e241d8e53587bf6d54d0dda49b9b9142f87e493
sidney gerst <you@example.com>
2020-03-09 14:46:27 -0700

    edited the markdown and organized assignment 2 folder

--- Assignments/Assignment2/fishfunction.R
+++ Assignments/Assignment2/fishfunction.R
@@ -7,30 +7,8 @@
 #' @return most_frequent_location, most_frequent_fish, revenue_location, total_revenue, plot
 
 
-########## Set up function##############3
-#types of fish
-fish_array<- c("Halibut", "Red Snapper", "Tuna", "Big Guy", "Little Blue")
 
-#fish price
-fish_price<- c(100, 70, 90, 150, 90)
-
-#fish price by the type of fish
-fish_price<- matrix(c(fish_price), nrow = 5, ncol = 1, byrow = T, dimnames = list(c(fish_array), c("fish_price")))
-
-#create a sample of fish catches
-fish_catch<- sample(5:120, 100, replace = FALSE)
-
-#locations of fisheries
-fish_locations<-  c("Santa_Barbara", "Long_Beach", "Santa_Cruz", "Portland")
-
-#final matrix that has the number of fish catch and price by the fish array
-fish_catch_location<- matrix(c(fish_catch), nrow = 5, ncol = 4, dimnames = list(c(fish_array), c(fish_locations)))
-
-fish_catch_location
-
-############Function##################
-
-fish_function<- function(fish_catch, fish_price, plot = F){
+fishfunction<- function(fish_catch, fish_price, plot = F){
   
   #find the most frequent fish per location
   most_frequent = list(colnames(fish_catch_location), rownames(fish_catch_location)[apply(fish_catch_location, 2, which.max)])
@@ -49,8 +27,10 @@ fish_function<- function(fish_catch, fish_price, plot = F){
     fish_plot<- ggplot(location_revenue_df, aes(x = location, y = location_revenue, fill = location))+
       geom_bar(stat = "identity")+
       theme_bw()+
+      scale_y_continuous(expand = expand_scale(mult = c(0,0.05))) +
       theme(legend.position = "none")+
       labs(title = "Fisheries Revenue",
+           subtitle = sprintf("Total revenue is $%d", total_revenue),
            x = "Location",
            y = "Total Revenue ($)")
   }
